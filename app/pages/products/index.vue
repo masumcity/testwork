6045324f426d363bf746506fa78ac52e1cb2f67c
Masum <masum@Masums-Mac-mini.local>
2026-01-20 17:23:04 +0600

    test 3

--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -1,3 +1,15 @@
+<script setup>
+const { data, pending, error } = await useFetch(
+  "https://admindash.comcitybd.com/api/test-open",
+  {
+    default: () => [],
+  },
+);
+</script>
+
 <template>
-  <h3>categories Index</h3>
+  <h3>test Index</h3>
+  <p>{{ data.ip }}</p>
+  <p>{{ data.ok }}</p>
+  <p>{{ data.ua }}</p>
 </template>
